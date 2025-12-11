760da6944e1c8dd9ea64b444d71427559d9facea
Shadowbeasto7 <126560486+Shadowbeasto7@users.noreply.github.com>
2025-12-11 17:54:01 +0545

    draw

--- day1/main.js
+++ day1/main.js
@@ -20,35 +20,191 @@ const ctx=canvas.getContext("2d");
 // ctx.lineTo(200, 100);
 // ctx.stroke();
 
+// ctx.beginPath();
+// ctx.rect(10,10,70,200);
+;
+// ctx.fillStyle = "black";
+
+
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.rect(33,10,25,250);
+// ctx.fillStyle = "black";
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.arc(45,50,20,0,2*Math.PI);
+//  ctx.fill();
+//  ctx.fillStyle="red";
+//  ctx.fill();
+
+//  ctx.beginPath();
+//  ctx.arc(45,100,20,0,2*Math.PI);
+//  ctx.fill();
+//  ctx.fillStyle="yellow";
+//  ctx.fill();
+
+//  ctx.beginPath();
+//  ctx.arc(45,150,20,0,2*Math.PI);
+//  ctx.fill();
+//  ctx.fillStyle="green";
+//  ctx.fill();
+
 ctx.beginPath();
-ctx.rect(10,10,70,200);
-ctx.fillStyle = "black";
-
-
+ctx.rect(0,360,500,500);
+ctx.fillStyle = "lightgreen";
 ctx.fill();
 
 ctx.beginPath();
-ctx.rect(33,10,25,250);
-ctx.fillStyle = "black";
+ctx.rect(200,200,200,180);
+ctx.fillStyle = "brown";
+ctx.lineWidth = 3;
+ctx.stroke(); 
+
+ctx.fill()
+
+ctx.beginPath();
+ctx.rect(230,230,20,19);
+ctx.fillStyle = "gray";
+ctx.strokeStyle="black";
+ctx.stroke()
+ctx.fill()
+
+ctx.beginPath();
+ctx.rect(230,250,20,20);
+ctx.fillStyle = "gray";
+ctx.strokeStyle="black";
+ctx.stroke()
+ctx.fill()
+
+ctx.beginPath();
+ctx.rect(250,250,20,20);
+ctx.fillStyle = "gray";
+ctx.strokeStyle="black";
+ctx.stroke()
+ctx.fill()
+
+ctx.beginPath();
+ctx.rect(250,230,20,19);
+ctx.fillStyle = "gray";
+ctx.strokeStyle="black";
+ctx.stroke()
+ctx.fill()
+
+
+ctx.beginPath();
+ctx.rect(350,230,20,19);
+ctx.fillStyle = "gray";
+ctx.strokeStyle="black";
+ctx.stroke()
+ctx.fill()
+
+ctx.beginPath();
+ctx.rect(350,250,20,20);
+ctx.fillStyle = "gray";
+ctx.strokeStyle="black";
+ctx.stroke()
+ctx.fill()
+
+ctx.beginPath();
+ctx.rect(330,250,20,20);
+ctx.fillStyle = "gray";
+ctx.strokeStyle="black";
+ctx.stroke()
+ctx.fill()
+
+ctx.beginPath();
+ctx.rect(330,230,20,19);
+ctx.fillStyle = "gray";
+ctx.strokeStyle="black";
+ctx.stroke()
+ctx.fill()
+
+ctx.beginPath();
+ctx.rect(275,300,50,78);
+ctx.fillStyle = "Coffee";
+ctx.strokeStyle="black";
+ctx.stroke()
+ctx.fill()
+
+// ctx.beginPath();
+// ctx.rect(20,150,5,78);
+// ctx.fillStyle = "gray";
+// ctx.strokeStyle="black";
+// ctx.stroke()
+// ctx.fill()
+
+ctx.beginPath();
+ctx.arc(70,90,35,0,2*Math.PI);
+ctx.fill();
+ctx.fillStyle="orange";
+ctx.strokeStyle="black";
+ctx.stroke()
 ctx.fill();
 
 ctx.beginPath();
-ctx.arc(45,50,20,0,2*Math.PI);
- ctx.fill();
- ctx.fillStyle="red";
- ctx.fill();
+ctx.arc(320,340,3,0,2*Math.PI);
+ctx.fill();
+ctx.fillStyle="yellow";
+ctx.strokeStyle="black";
+ctx.stroke()
+ctx.fill();
 
- ctx.beginPath();
- ctx.arc(45,100,20,0,2*Math.PI);
- ctx.fill();
- ctx.fillStyle="yellow";
- ctx.fill();
+ctx.beginPath();
+ctx.rect(270,378,60,5);
+ctx.fillStyle = "gray";
+ctx.strokeStyle="black";
+ctx.stroke()
+ctx.fill()
 
- ctx.beginPath();
- ctx.arc(45,150,20,0,2*Math.PI);
- ctx.fill();
- ctx.fillStyle="green";
- ctx.fill();
+ctx.beginPath();
+ctx.moveTo(120,100);
+ctx.lineTo(140, 103);
+ctx.strokeStyle="orange";
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(25,90);
+ctx.lineTo(6, 89);
+ctx.strokeStyle="orange";
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.moveTo(25,90);
+ctx.lineTo(6, 89);
+ctx.strokeStyle="orange";
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(72,153);
+ctx.lineTo(71, 132);
+ctx.strokeStyle="orange";
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(68,45);
+ctx.lineTo(67, 20);
+ctx.strokeStyle="orange";
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.moveTo(180, 200);   
+ctx.lineTo(300, 120);  
+ctx.lineTo(420, 200); 
+ctx.closePath();       
+
+ctx.strokeStyle = "black"; 
+ctx.lineWidth = 3;
+ctx.stroke();         
+
+ctx.fillStyle = "lightblue";
+ctx.fill();
+
+
+
 
  
 
